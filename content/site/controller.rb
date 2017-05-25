94bf4d48985b0e708a51d0076b4a25f241c65b62
Pontus <pontus.sundqvist@itggot.se>
2017-05-25 11:27:04 +0200

    Cleaning up

--- content/site/controller.rb
+++ content/site/controller.rb
@@ -1,11 +1,7 @@
 layout 'layout.html.erb'
 
-
 before 'index.html.erb' do
   @articles = Dir.entries("../articles")
-  p @articles
   @articles = @articles.slice!(2..11)
-  p @articles
-
   @articles.sort! { |x,y| File.birthtime("../articles/#{y}") <=> File.birthtime("../articles/#{x}")}
 end
